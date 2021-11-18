74f09415170085dba04db21c21720ce73d953098
t1l7y09 <94492106+t1l7y09@users.noreply.github.com>
2021-11-18 03:13:11 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'TiltyFN'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2021, TiltyFN'
+author = 'TiltyFN'
 
 release = '0.1'
 version = '0.1.0'
